ea62e637e98b24544f134a6af950e6bbf56b9640
mJunaid14 <muhammad.junaid@freit.io>
2023-05-17 10:29:56 +0500

    -Added local nodemon dependency

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -22,86 +22,86 @@ import {
 import Project from "../Projects/Project";
 
 const Home = () => {
-  // useEffect(() => {
-  //   const textureloader = new THREE.TextureLoader();
+  useEffect(() => {
+    const textureloader = new THREE.TextureLoader();
 
-  //   const moonsTexture = textureloader.load(moonImg);
-  //   const venusTexture = textureloader.load(venusIMG);
-  //   const spaceTexture = textureloader.load(spaceIMG);
+    const moonsTexture = textureloader.load(moonImg);
+    const venusTexture = textureloader.load(venusIMG);
+    const spaceTexture = textureloader.load(spaceIMG);
 
-  //   const scene = new THREE.Scene();
-  //   const camera = new THREE.PerspectiveCamera(
-  //     60,
-  //     window.innerWidth / window.innerHeight,
-  //     0.1,
-  //     1000
-  //   );
-  //   camera.position.set(4, 4, 8);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      60,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.set(4, 4, 8);
 
-  //   const canvas = document.querySelector(".homeCanvas");
-  //   const renderer = new THREE.WebGLRenderer({ canvas });
+    const canvas = document.querySelector(".homeCanvas");
+    const renderer = new THREE.WebGLRenderer({ canvas });
 
-  //   const moonGeometry = new THREE.SphereGeometry(2, 64, 64);
-  //   const moonMaterial = new THREE.MeshStandardMaterial({ map: moonsTexture });
-  //   const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+    const moonGeometry = new THREE.SphereGeometry(2, 64, 64);
+    const moonMaterial = new THREE.MeshStandardMaterial({ map: moonsTexture });
+    const moon = new THREE.Mesh(moonGeometry, moonMaterial);
 
-  //   const venusGeometry = new THREE.SphereGeometry(3, 64, 64);
-  //   const venusMaterial = new THREE.MeshBasicMaterial({ map: venusTexture });
-  //   const venus = new THREE.Mesh(venusGeometry, venusMaterial);
-  //   venus.position.set(8, 5, 5);
+    const venusGeometry = new THREE.SphereGeometry(3, 64, 64);
+    const venusMaterial = new THREE.MeshBasicMaterial({ map: venusTexture });
+    const venus = new THREE.Mesh(venusGeometry, venusMaterial);
+    venus.position.set(8, 5, 5);
 
-  //   const pointLight = new THREE.PointLight(0xffffff, 1);
-  //   const pointLight2 = new THREE.PointLight(0xffffff, 0.1);
-  //   pointLight.position.set(8, 5, 5);
-  //   pointLight2.position.set(-8, -5, -5);
+    const pointLight = new THREE.PointLight(0xffffff, 1);
+    const pointLight2 = new THREE.PointLight(0xffffff, 0.1);
+    pointLight.position.set(8, 5, 5);
+    pointLight2.position.set(-8, -5, -5);
 
-  //   // const control = new OrbitControls(camera, renderer.domElement)
+    // const control = new OrbitControls(camera, renderer.domElement)
 
-  //   scene.add(moon);
-  //   scene.add(pointLight);
-  //   scene.add(pointLight2);
-  //   scene.add(venus);
-  //   scene.background = spaceTexture;
+    scene.add(moon);
+    scene.add(pointLight);
+    scene.add(pointLight2);
+    scene.add(venus);
+    scene.background = spaceTexture;
 
-  //   const constSpeed = 0.01;
-  //   window.addEventListener("mousemove", (e) => {
-  //     if (e.clientX <= window.innerWidth / 2) {
-  //       moon.rotation.x -= constSpeed;
-  //       moon.rotation.y += constSpeed;
-  //       venus.rotation.x -= constSpeed;
-  //       venus.rotation.y += constSpeed;
-  //     }
+    const constSpeed = 0.01;
+    window.addEventListener("mousemove", (e) => {
+      if (e.clientX <= window.innerWidth / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y += constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y += constSpeed;
+      }
 
-  //     if (e.clientX > window.innerWidth / 2) {
-  //       moon.rotation.x -= constSpeed;
-  //       moon.rotation.y -= constSpeed;
-  //       venus.rotation.x -= constSpeed;
-  //       venus.rotation.y -= constSpeed;
-  //     }
-  //     if (e.clientY <= window.innerHeight / 2) {
-  //       moon.rotation.x -= constSpeed;
-  //       moon.rotation.y += constSpeed;
-  //       venus.rotation.x -= constSpeed;
-  //       venus.rotation.y += constSpeed;
-  //     }
-  //     if (e.clientY > window.innerHeight / 2) {
-  //       moon.rotation.x -= constSpeed;
-  //       moon.rotation.y -= constSpeed;
-  //       venus.rotation.x -= constSpeed;
-  //       venus.rotation.y -= constSpeed;
-  //     }
-  //   });
+      if (e.clientX > window.innerWidth / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y -= constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+      if (e.clientY <= window.innerHeight / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y += constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y += constSpeed;
+      }
+      if (e.clientY > window.innerHeight / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y -= constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+    });
 
-  //   const animate = () => {
-  //     requestAnimationFrame(animate);
+    const animate = () => {
+      requestAnimationFrame(animate);
 
-  //     moon.rotation.y += 0.001;
-  //     venus.rotation.y += 0.001;
-  //     renderer.setSize(window.innerWidth, window.innerHeight);
-  //     renderer.render(scene, camera);
-  //   };
-  //   animate();
-  // }, []);
+      moon.rotation.y += 0.001;
+      venus.rotation.y += 0.001;
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.render(scene, camera);
+    };
+    animate();
+  }, []);
 
   return (
     <>
