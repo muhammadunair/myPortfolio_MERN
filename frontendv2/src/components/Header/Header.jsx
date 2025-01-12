093a04baeed9ae98224c894c1f132da3273c8a3b
muhammadunair <unairur@gmail.com>
2025-01-12 16:53:00 +0500

    -fixed header logout issue

--- frontendv2/src/components/Header/Header.jsx
+++ frontendv2/src/components/Header/Header.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "../../images/logo.png"
 import { FaUserAlt } from 'react-icons/fa'
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../Action/Action";
 
 const Header = () => {
+  const dispatch = useDispatch();
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const { isAuthenticated } = useSelector((state) => state.login);
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+
   return (
     <header>
-      <nav className="sticky bg-black border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <nav className="sticky !bg-black border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
             <img src={logo} className="mr-3 h-16 sm:h-36" alt="Flowbite Logo" />
           </Link>
           <div className="flex items-center lg:order-2">
+            {
+              isAuthenticated ? 
+              <>
+              <Link to="/account" className="text-white dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Account</Link>
+              <Link className="text-white dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800" onClick={logoutHandler}>Logout</Link>
+              </>
+            :
             <Link to="/login" className="text-white dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</Link>
+            }
             <button onClick={() => setIsMenuOpen(!isMenuOpen)} data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-black dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
